91c4414dd06bbd9564aef8d6304133a2683962a9
keith <keith@raincityambience.com>
2017-11-29 23:08:03 -0800

    change url from store input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,35 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
 
+  constructor(){
+    super();
+    this.goToStore = this.goToStore.bind(this)
+  }
+
+  goToStore(e){
+    e.preventDefault();
+    //first grab text from box
+    console.log(this.storeInput.value);
+    //second transition from / to /store/id
+    this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input ref={(input)=>{this.storeInput = input}} type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     )
   }
 }
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
 
 export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,26 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import App from './components/App';
-// import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 import './css/style.css'
 
+const Root = () => {
+  return(
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
 
-render(<App />, document.getElementById('main'));
+    </BrowserRouter>
+  )
+
+}
+
+render(<Root/>, document.getElementById('main'));
